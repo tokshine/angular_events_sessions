539a733bdbdce7b5013c75bbca9315295f91c73a
Adeleke, Adetokunbo <Adetokunbo.Adeleke@ehi.com>
2020-03-13 13:26:00 +0000

    isolated and end to end test

---
+++ src/app/test.spec.ts
@@ -0,0 +1,10 @@
+describe('first test',()=>{
+    it('should be true',()=>{
+        expect(true).toBe(true);
+    })
+
+    it('should be false',()=>{
+        expect(false).toBe(false);
+    })
+
+})
